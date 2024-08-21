b7ccb4e62e81cbc96051e7775d4c667918c17526
DanielEric08 <103253330+DanielEric08@users.noreply.github.com>
2024-08-21 13:38:25 +0800

    Add files via upload

---
+++ Codemaster/about.php
@@ -0,0 +1,88 @@
+<?php
+
+include 'components/connect.php';
+
+if(isset($_COOKIE['user_id'])){
+   $user_id = $_COOKIE['user_id'];
+}else{
+   $user_id = '';
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<?php include 'components/user_header.php'; ?>
+
+<!-- about section starts  -->
+
+<section class="about">
+
+   <div class="row">
+
+      <div class="image">
+         <img src="images/about-img.svg" alt="">
+      </div>
+
+      <div class="content">
+         <h3>why choose us?</h3>
+         <p>CodeMaster is an innovative web application designed to empower students in mastering various programming languages. Through a comprehensive suite of courses, CodeMaster provides an engaging and interactive learning experience that caters to beginners and advanced learners alike. With meticulously structured content, practical coding exercises, and real-time feedback, students can seamlessly develop their programming skills. CodeMaster's user-friendly interface and supportive community further enhance the learning journey, making it an indispensable tool for anyone looking to excel in the world of programming.</p>
+         <a href="courses.html" class="inline-btn">our courses</a>
+      </div>
+
+   </div>
+
+   <div class="box-container">
+
+      <div class="box">
+         <i class="fas fa-graduation-cap"></i>
+         <div>
+            <h3>10</h3>
+            <span>online courses</span>
+         </div>
+      </div>
+
+      <div class="box">
+         <i class="fas fa-user-graduate"></i>
+         <div>
+            <h3></h3>
+            <span>brilliants students</span>
+         </div>
+      </div>
+
+      <div class="box">
+         <i class="fas fa-chalkboard-user"></i>
+         <div>
+            <h3>5</h3>
+            <span>expert teachers</span>
+         </div>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- about section ends -->
+
+<?php include 'components/footer.php'; ?>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+   
+</body>
+</html>
---
+++ Codemaster/components/footer.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+
+    &copy; copyright @ <?= date('Y'); ?> by <span>CodeMaster</span> | all rights reserved!
+ 
+ </footer>
